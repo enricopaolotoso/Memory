de0d42cc489a7b67ae04535c2351506c5a936423
enricopaolotoso <74261261+enricopaolotoso@users.noreply.github.com>
2021-10-14 10:17:50 +0200

    upload file

---
+++ Memory1/Memory1/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace Memory1
+{
+    partial class Memory
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.carta1 = new System.Windows.Forms.PictureBox();
+            this.nturno = new System.Windows.Forms.Label();
+            this.carta2 = new System.Windows.Forms.PictureBox();
+            this.carta3 = new System.Windows.Forms.PictureBox();
+            this.carta4 = new System.Windows.Forms.PictureBox();
+            this.carta5 = new System.Windows.Forms.PictureBox();
+            this.carta6 = new System.Windows.Forms.PictureBox();
+            this.carta7 = new System.Windows.Forms.PictureBox();
+            this.carta8 = new System.Windows.Forms.PictureBox();
+            this.restartGame = new System.Windows.Forms.Button();
+            this.scrittaMemory = new System.Windows.Forms.Label();
+            this.dNickname = new System.Windows.Forms.TextBox();
+            this.nikname1 = new System.Windows.Forms.TextBox();
+            this.nikname2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.carta1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // carta1
+            // 
+            this.carta1.Location = new System.Drawing.Point(47, 53);
+            this.carta1.Name = "carta1";
+            this.carta1.Size = new System.Drawing.Size(150, 190);
+            this.carta1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.carta1.TabIndex = 0;
+            this.carta1.TabStop = false;
+            this.carta1.Click += new System.EventHandler(this.carta1_Click);
+            // 
+            // nturno
+            // 
+            this.nturno.AutoSize = true;
+            this.nturno.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nturno.Location = new System.Drawing.Point(683, 20);
+            this.nturno.Name = "nturno";
+            this.nturno.Size = new System.Drawing.Size(0, 20);
+            this.nturno.TabIndex = 1;
+            // 
+            // carta2
+            // 
+            this.carta2.Location = new System.Drawing.Point(228, 53);
+            this.carta2.Name = "carta2";
+            this.carta2.Size = new System.Drawing.Size(150, 190);
+            this.carta2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.carta2.TabIndex = 2;
+            this.carta2.TabStop = false;
+            this.carta2.Click += new System.EventHandler(this.carta2_Click);
+            // 
+            // carta3
+            // 
+            this.carta3.Location = new System.Drawing.Point(408, 53);
+            this.carta3.Name = "carta3";
+            this.carta3.Size = new System.Drawing.Size(150, 190);
+            this.carta3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.carta3.TabIndex = 3;
+            this.carta3.TabStop = false;
+            this.carta3.Click += new System.EventHandler(this.carta3_Click);
+            // 
+            // carta4
+            // 
+            this.carta4.Location = new System.Drawing.Point(588, 53);
+            this.carta4.Name = "carta4";
+            this.carta4.Size = new System.Drawing.Size(150, 190);
+            this.carta4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.carta4.TabIndex = 4;
+            this.carta4.TabStop = false;
+            this.carta4.Click += new System.EventHandler(this.carta4_Click);
+            // 
+            // carta5
+            // 
+            this.carta5.Location = new System.Drawing.Point(47, 281);
+            this.carta5.Name = "carta5";
+            this.carta5.Size = new System.Drawing.Size(150, 190);
+            this.carta5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.carta5.TabIndex = 5;
+            this.carta5.TabStop = false;
+            this.carta5.Click += new System.EventHandler(this.carta5_Click);
+            // 
+            // carta6
+            // 
+            this.carta6.Location = new System.Drawing.Point(228, 281);
+            this.carta6.Name = "carta6";
+            this.carta6.Size = new System.Drawing.Size(150, 190);
+            this.carta6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.carta6.TabIndex = 6;
+            this.carta6.TabStop = false;
+            this.carta6.Click += new System.EventHandler(this.carta6_Click);
+            // 
+            // carta7
+            // 
+            this.carta7.Location = new System.Drawing.Point(408, 281);
+            this.carta7.Name = "carta7";
+            this.carta7.Size = new System.Drawing.Size(150, 190);
+            this.carta7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.carta7.TabIndex = 7;
+            this.carta7.TabStop = false;
+            this.carta7.Click += new System.EventHandler(this.carta7_Click);
+            // 
+            // carta8
+            // 
+            this.carta8.Location = new System.Drawing.Point(588, 281);
+            this.carta8.Name = "carta8";
+            this.carta8.Size = new System.Drawing.Size(150, 190);
+            this.carta8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.carta8.TabIndex = 8;
+            this.carta8.TabStop = false;
+            this.carta8.Click += new System.EventHandler(this.carta8_Click);
+            // 
+            // restartGame
+            // 
+            this.restartGame.Font = new System.Drawing.Font("Haettenschweiler", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restartGame.Location = new System.Drawing.Point(300, 489);
+            this.restartGame.Name = "restartGame";
+            this.restartGame.Size = new System.Drawing.Size(167, 32);
+            this.restartGame.TabIndex = 9;
+            this.restartGame.Text = "Gioca";
+            this.restartGame.UseVisualStyleBackColor = true;
+            this.restartGame.Click += new System.EventHandler(this.restartGame_Click);
+            // 
+            // scrittaMemory
+            // 
+            this.scrittaMemory.AutoSize = true;
+            this.scrittaMemory.BackColor = System.Drawing.Color.Transparent;
+            this.scrittaMemory.Font = new System.Drawing.Font("MV Boli", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scrittaMemory.ForeColor = System.Drawing.Color.Red;
+            this.scrittaMemory.Location = new System.Drawing.Point(181, 37);
+            this.scrittaMemory.Name = "scrittaMemory";
+            this.scrittaMemory.Size = new System.Drawing.Size(416, 125);
+            this.scrittaMemory.TabIndex = 10;
+            this.scrittaMemory.Text = "Memory";
+            // 
+            // dNickname
+            // 
+            this.dNickname.Location = new System.Drawing.Point(302, 193);
+            this.dNickname.Name = "dNickname";
+            this.dNickname.Size = new System.Drawing.Size(179, 20);
+            this.dNickname.TabIndex = 11;
+            this.dNickname.Text = "Inserisci nick name ";
+            // 
+            // nikname1
+            // 
+            this.nikname1.Location = new System.Drawing.Point(302, 233);
+            this.nikname1.Name = "nikname1";
+            this.nikname1.Size = new System.Drawing.Size(177, 20);
+            this.nikname1.TabIndex = 12;
+            // 
+            // nikname2
+            // 
+            this.nikname2.Location = new System.Drawing.Point(304, 269);
+            this.nikname2.Name = "nikname2";
+            this.nikname2.Size = new System.Drawing.Size(177, 20);
+            this.nikname2.TabIndex = 14;
+            // 
+            // Memory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.nikname2);
+            this.Controls.Add(this.nikname1);
+            this.Controls.Add(this.dNickname);
+            this.Controls.Add(this.scrittaMemory);
+            this.Controls.Add(this.restartGame);
+            this.Controls.Add(this.carta8);
+            this.Controls.Add(this.carta7);
+            this.Controls.Add(this.carta6);
+            this.Controls.Add(this.carta5);
+            this.Controls.Add(this.carta4);
+            this.Controls.Add(this.carta3);
+            this.Controls.Add(this.carta2);
+            this.Controls.Add(this.nturno);
+            this.Controls.Add(this.carta1);
+            this.Name = "Memory";
+            this.Text = "Memory";
+            ((System.ComponentModel.ISupportInitialize)(this.carta1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carta8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox carta1;
+        private System.Windows.Forms.Label nturno;
+        private System.Windows.Forms.PictureBox carta2;
+        private System.Windows.Forms.PictureBox carta3;
+        private System.Windows.Forms.PictureBox carta4;
+        private System.Windows.Forms.PictureBox carta5;
+        private System.Windows.Forms.PictureBox carta6;
+        private System.Windows.Forms.PictureBox carta7;
+        private System.Windows.Forms.PictureBox carta8;
+        private System.Windows.Forms.Button restartGame;
+        private System.Windows.Forms.Label scrittaMemory;
+        private System.Windows.Forms.TextBox dNickname;
+        private System.Windows.Forms.TextBox nikname1;
+        private System.Windows.Forms.TextBox nikname2;
+    }
+}
+
---
+++ Memory1/Memory1/Form1.cs
@@ -0,0 +1,416 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory1
+{
+    public partial class Memory : Form
+    {
+        public Memory()
+        {
+            InitializeComponent();
+            
+        }
+        string[] animationCarte = new string[8];
+        bool turno;
+        bool[] sceltaGiocatore = new bool[] { false, false, false, false, false, false, false, false };
+        string player1;
+        string player2;
+        int carteSelezionate = 0, coppie1 = 0, coppie2 = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            startGame();
+        }
+
+        private void startGame()
+        {
+            sceltaTurno();
+            string[] Animation = new string[] { "troll", "sax", "pirate", "superman" };
+            int nGenTroll = 0, nGenSax = 0, nGenPirate = 0, nGenSuperman = 0;
+            Random genCarte = new Random();
+            for (int i = 0; i < 8; i++)
+            {
+                string animationGenerato
+                    = genCarteRandom(Animation, genCarte, ref nGenTroll, ref nGenSax, ref nGenPirate, ref nGenSuperman);
+                //controlla se il fiore generato è stato sorteggiato già più di due volte
+                if (i > 1 && controlloCarte(animationCarte, ref animationGenerato, ref nGenTroll, ref nGenSax, ref nGenPirate, ref nGenSuperman) == -1)
+                {
+                    i--;//torna indietro di un ciclo, per permettere quando il ciclo si conclude di ripetere
+                        //dopo lo stesso ciclo per rigenerare un altro fiore per la medesima posizione nell'array
+                }
+                else
+                {
+                    animationCarte[i] = animationGenerato;
+                }
+            }
+            getSfondoCarte();
+        }
+        //viene generato randomicamente il turno del giocatore
+        private void sceltaTurno()
+        {
+            Random turnoRnd = new Random();
+            int scelta = turnoRnd.Next(0, 2);
+            if (scelta == 0)
+            {
+                turno = false;//giocatore 1
+                this.BackColor = Color.RoyalBlue;
+                nturno.Text = player1;
+            }
+            else
+            {
+                turno = true;//giocatore 2
+                this.BackColor = Color.GreenYellow;
+                nturno.Text = player2;
+            }
+        }
+        //assegna alle picturebox l'immagine iniziale
+        private void getSfondoCarte()
+        {
+            carta1.Image = Properties.Resources.qd;
+            carta2.Image = Properties.Resources.qd;
+            carta3.Image = Properties.Resources.qd;
+            carta4.Image = Properties.Resources.qd;
+            carta5.Image = Properties.Resources.qd;
+            carta6.Image = Properties.Resources.qd;
+            carta7.Image = Properties.Resources.qd;
+            carta8.Image = Properties.Resources.qd;
+        }
+        //genera un'immagine random
+        private string genCarteRandom(string[] arraycarte, Random gnCard, ref int nGenptroll, ref int nGensax, ref int nGenpirate, ref int nGensuperman)
+        {
+            string animationGenerato = "";
+            int posGenerata = 0;
+            posGenerata = gnCard.Next(0, 4);
+            animationGenerato = arraycarte[posGenerata];
+            switch (animationGenerato)
+            {
+                case "troll":
+                    nGenptroll = aumentoNumFioreGenerato(nGenptroll);
+                    break;
+                case "sax":
+                    nGensax = aumentoNumFioreGenerato(nGensax);
+                    break;
+                case "pirate":
+                    nGenpirate = aumentoNumFioreGenerato(nGenpirate);
+                    break;
+                case "superman":
+                    nGensuperman = aumentoNumFioreGenerato(nGensuperman);
+                    break;
+            }
+            return animationGenerato;
+        }
+        //controlla se il fiore è già stato generato più di due volte
+        private int controlloCarte(string[] arrayCard, ref string animationGenerato, ref int nGentroll, ref int nGensax, ref int nGenpirate, ref int nGensuperman)
+        {
+            //per togliere i valori nulli
+            arrayCard = arrayCard.Where(c => c != null).ToArray();
+            for (int i = 0; i < arrayCard.Length; i++)
+            {
+                if (arrayCard[i] == animationGenerato)
+                {
+                    switch (animationGenerato)
+                    {
+                        case "troll":
+                            if (nGentroll == 3)
+                            {
+                                return -1;
+                            }
+                            break;
+                        case "sax":
+                            if (nGensax == 3)
+                            {
+                                return -1;
+                            }
+                            break;
+                        case "tiktok":
+                            if (nGenpirate == 3)
+                            {
+                                return -1;
+                            }
+                            break;
+                        case "nGensuperman":
+                            if (nGensuperman == 3)
+                            {
+                                return -1;
+                            }
+                            break;
+                    }
+                }
+            }
+            return 1;
+        }
+        //controlla per tutte le variabili se sono stati sorteggiati più di due volte
+        private int aumentoNumFioreGenerato(int genNum)
+        {
+            if (genNum < 3)
+            {
+                genNum++;
+            }
+            return genNum;
+        }
+        //funzione al clikc di una carta
+        private void clickCarta(int numCarta)
+        {
+            numCarta -= 1; //per conformarlo alla posizione dell'array
+            if (sceltaGiocatore[numCarta] == false)
+            {
+                switch (numCarta)
+                {
+                    case 0:
+                        carta1.Image = immaginecarta(numCarta);
+                        sceltaGiocatore[numCarta] = true;
+                        controlloTurno(numCarta);
+                        break;
+                    case 1:
+                        carta2.Image = immaginecarta(numCarta);
+                        sceltaGiocatore[numCarta] = true;
+                        controlloTurno(numCarta);
+                        break;
+                    case 2:
+                        carta3.Image = immaginecarta(numCarta);
+                        sceltaGiocatore[numCarta] = true;
+                        controlloTurno(numCarta);
+                        break;
+                    case 3:
+                        carta4.Image = immaginecarta(numCarta);
+                        sceltaGiocatore[numCarta] = true;
+                        controlloTurno(numCarta);
+                        break;
+                    case 4:
+                        carta5.Image = immaginecarta(numCarta);
+                        sceltaGiocatore[numCarta] = true;
+                        controlloTurno(numCarta);
+                        break;
+                    case 5:
+                        carta6.Image = immaginecarta(numCarta);
+                        sceltaGiocatore[numCarta] = true;
+                        controlloTurno(numCarta);
+                        break;
+                    case 6:
+                        carta7.Image = immaginecarta(numCarta);
+                        sceltaGiocatore[numCarta] = true;
+                        controlloTurno(numCarta);
+                        break;
+                    case 7:
+                        carta8.Image = immaginecarta(numCarta);
+                        sceltaGiocatore[numCarta] = true;
+                        controlloTurno(numCarta);
+                        break;
+                }
+            }
+        }
+        //assegna alle carte un immagine a seconda dell'array generato
+        private System.Drawing.Image immaginecarta(int nCarta)
+        {
+            if (animationCarte[nCarta] == "troll")
+            {
+                return Properties.Resources.troll;
+            }
+            else if (animationCarte[nCarta] == "pirate")
+            {
+                return Properties.Resources.pirate;
+            }
+            else if (animationCarte[nCarta] == "sax")
+            {
+                return Properties.Resources.sax;
+            }
+            else if (animationCarte[nCarta] == "superman")
+            {
+                return Properties.Resources.superman;
+            }
+            return null;
+        }
+        private void controlloTurno(int nCard)
+        {
+            carteSelezionate++;
+            controlloCoppie(ref nCard);
+        }
+        int card1, card2;
+        private int nGensax;
+
+        private void controlloCoppie(ref int nCard)
+        {
+            if (carteSelezionate % 2 == 0)
+            {
+                card2 = nCard;
+                if (animationCarte[card1] != animationCarte[card2])
+                {
+                    sceltaGiocatore[card1] = false;
+                    sceltaGiocatore[card2] = false;
+                    wait(500);//fa aspettare 0,5s all'operazione successiva
+                    assegnazioneSfondoCarteNonCoppie(card1);
+                    assegnazioneSfondoCarteNonCoppie(card2);
+                    carteSelezionate -= 2;
+                    turno = !turno;//cambia il valore da true a false o da false a true
+                    if (turno)
+                    {
+                        this.BackColor = Color.Beige;
+                        nturno.Text = player2;
+                    }
+                    else
+                    {
+                        this.BackColor = Color.DarkOliveGreen;
+                        nturno.Text = player1;
+                    }
+                }
+                else
+                {
+                    if (turno)//assegna il punteggio a seconda del turno
+                    {
+                        coppie2++;
+                    }
+                    else
+                    {
+                        coppie1++;
+                    }
+                }
+            }
+            else
+            {
+                card1 = nCard;
+            }
+            if (carteSelezionate == 8)//mostra il tasto per ricominciare a giocare
+            {
+                restartGame.Visible = true;
+                this.BackColor = Color.White;
+                if (coppie1 > coppie2)
+                {
+                    MessageBox.Show($"La partita si è conclusa\nHa vinto "+ player1);
+                }
+                else
+                {
+                    MessageBox.Show($"La partita si è conclusa\nHa vinto "+ player2);
+                }
+                //è impossible fare un pareggio con 4 coppie
+            }
+        }
+
+        private void assegnazioneSfondoCarteNonCoppie(int cardNum)
+        {
+            switch (cardNum)
+            {
+                case 0:
+                    carta1.Image = Properties.Resources.qd;
+                    break;
+                case 1:
+                    carta2.Image = Properties.Resources.qd;
+                    break;
+                case 2:
+                    carta3.Image = Properties.Resources.qd;
+                    break;
+                case 3:
+                    carta4.Image = Properties.Resources.qd;
+                    break;
+                case 4:
+                    carta5.Image = Properties.Resources.qd;
+                    break;
+                case 5:
+                    carta6.Image = Properties.Resources.qd;
+                    break;
+                case 6:
+                    carta7.Image = Properties.Resources.qd;
+                    break;
+                case 7:
+                    carta8.Image = Properties.Resources.qd;
+                    break;
+            }
+        }
+
+        public void wait(int milliseconds)
+        {
+            //crea un oggetto Timer
+            var timer1 = new System.Windows.Forms.Timer();
+            //controlla che il valore dei millisecondi non sia 0 o minore di 0
+            if (milliseconds == 0 || milliseconds < 0) return;
+
+            // inizia il timer
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            //controlla se il valore inserito in millisecondi è trascorso
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+                // finisce il timer
+            };
+
+            while (timer1.Enabled)//permette di elaborare le azioni che il form riceve
+            {
+                Application.DoEvents();
+            }
+        }
+
+
+        private void restartGame_Click(object sender, EventArgs e)
+        {
+            if (nikname1.Text==""||nikname2.Text=="")
+            {
+                MessageBox.Show("Inserisci nick name");
+            }
+            else
+            {
+                player1 = nikname1.Text;
+                player2 = nikname2.Text;
+                nikname1.Visible = false;
+                nikname2.Visible = false;
+                dNickname.Visible = false;
+                carteSelezionate = 0;
+                card1 = 0; card2 = 0;
+                for (int i = 0; i < 8; i++) { sceltaGiocatore[i] = false; }
+                coppie1 = 0; coppie2 = 0;
+                startGame();
+                restartGame.Visible = false;
+                scrittaMemory.Visible = false;
+            }
+
+        }
+
+        private void carta1_Click(object sender, EventArgs e)
+        {
+            clickCarta(1);
+        }
+
+        private void carta2_Click(object sender, EventArgs e)
+        {
+            clickCarta(2);
+        }
+
+        private void carta3_Click(object sender, EventArgs e)
+        {
+            clickCarta(3);
+        }
+
+        private void carta4_Click(object sender, EventArgs e)
+        {
+            clickCarta(4);
+        }
+
+        private void carta5_Click(object sender, EventArgs e)
+        {
+            clickCarta(5);
+        }
+
+        private void carta6_Click(object sender, EventArgs e)
+        {
+            clickCarta(6);
+        }
+
+        private void carta7_Click(object sender, EventArgs e)
+        {
+            clickCarta(7);
+        }
+
+        private void carta8_Click(object sender, EventArgs e)
+        {
+            clickCarta(8);
+        }
+    }
+}
